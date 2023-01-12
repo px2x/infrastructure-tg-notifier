8cca50c17fa79f9b3740cb3cfc156e1919254b7c
px2x <it@sheverev.com>
2023-01-12 20:31:49 +0300

    [ITN-3][task] - add draft CheckAvailability fn

--- internal/tgbot/callbacks.go
+++ internal/tgbot/callbacks.go
@@ -4,9 +4,22 @@ import (
 	"context"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"github.com/px2x/infrastructure-tg-notifier/internal/app"
 )
 
 func (passer *DataPasser) callbackCheckHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	passer.app.Command <- app.Message{
+		Type:    update.CallbackQuery.Data,
+		Payload: "",
+		ChatID:  update.CallbackQuery.Message.Chat.ID,
+	}
+
+	//passer.app.Message <- app.Message{
+	//	Type:    update.CallbackQuery.Data,
+	//	Payload: "HI!",
+	//	ChatID:  update.CallbackQuery.Message.Chat.ID,
+	//}
+
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.CallbackQuery.Message.Chat.ID,
 		Text:   "You selected the button: " + update.CallbackQuery.Data,
